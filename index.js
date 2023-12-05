af9b564a398b386b83d78fec91c0e09edfb5bc92
AlexisHenry <alexis.henry150357@gmail.com>
2023-12-05 13:48:58 +0100

    feat: automatically update average using cron

---
+++ index.js
@@ -0,0 +1,29 @@
+const { z } = require('zod');
+const { promises: fs } = require('fs');
+const readme = require('./readme.js');
+
+const averageSchema = z.object({
+  average: z.number().min(0).max(20),
+});
+
+const updateReadme = (average) => {
+  const readmeContent = readme.replace(/{{average}}/g, average);
+
+  fs.writeFile('README.md', readmeContent)
+    .then(() => console.log('README.md updated'))
+    .catch((err) => console.error(err));
+}
+
+const main = () => {
+  fetch("https://epitech-grades.vercel.app/api/average")
+    .then(response => response.json())
+    .then(data => {
+      let isAverageValid = averageSchema.safeParse(data);
+
+      if (isAverageValid.success) {
+        updateReadme(isAverageValid.data.average);
+      }
+    });
+}
+
+main();
---
+++ readme.js
@@ -0,0 +1,60 @@
+module.exports = `# Hi :raised_hand:
+
+[![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white&color=0055A4)](https://www.linkedin.com/in/alexishenry03)
+[![Portfolio](https://img.shields.io/badge/portfolio-0077B5?style=for-the-badge&color=FFFFFF)](https://www.alexishenry.eu)
+[![Fiverr](https://img.shields.io/badge/fiverr-0077B5?style=for-the-badge&color=EF4135)](https://fr.fiverr.com/alexishenry363)
+
+I'm [Alexis](https://about.me/alexishenry), <strong>junior developer</strong> from Strasbourg, France  <img src="https://cdn-icons-png.flaticon.com/512/197/197560.png" width="14"/>.
+
+I'm a developer with a passion for exploring new programming languages and technologies. I believe that it is essential to have a broad understanding of different programming paradigms and practices in order to create effective solutions to complex problems.
+
+I am always eager to learn and improve my skills. My goal is to stay up-to-date with the latest trends and best practices in the tech industry, and to continuously expand my knowledge and capabilities.
+
+So if you're looking for a developer who is not afraid to take on new challenges and explore uncharted territory, look no further!
+
+My current average at Epitech is {{average}}/20 :wink:
+
+<img src="https://github-readme-stats.vercel.app/api?username=AlxisHenry&show_icons=true" />
+
+## Stack
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&color=20232a)
+![Bash](https://img.shields.io/badge/bash-%23121011.svg?style=for-the-badge&logo=gnu-bash&color=20232a)
+![Laravel](https://img.shields.io/badge/laravel-%23FF2D20.svg?style=for-the-badge&logo=laravel&color=20232a)
+![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&color=20232a)
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&color=20232a)
+![Next.JS](https://img.shields.io/badge/next-3670A0?style=for-the-badge&logo=nextdotjs&color=20232a)
+![React Native](https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+![Insomnia](https://img.shields.io/badge/Insomnia-black?style=for-the-badge&logo=insomnia&logoColor=5849BE&color=20232a)
+
+<!--
+![Symfony](https://img.shields.io/badge/symfony-%23000000.svg?style=for-the-badge&logo=symfony&color=20232a)
+![GitHub Actions](https://img.shields.io/badge/github%20actions-%232671E5.svg?style=for-the-badge&logo=githubactions&color=20232a)
+![Neovim](https://img.shields.io/badge/NeoVim-%2357A143.svg?&style=for-the-badge&logo=neovim&color=20232a)
+![AdonisJS](https://img.shields.io/badge/adonisjs%20-%23323330.svg?&style=for-the-badge&logo=adonisjs&color=20232a)
+![Docker](https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&color=20232a)
+![Vite](https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&color=20232a)
+![PNPM](https://img.shields.io/badge/pnpm-%234a4a4a.svg?style=for-the-badge&logo=pnpm&color=20232a)
+![Java](https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white&color=gray)
+![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white&color=gray)
+![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white&color=gray)
+![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white&color=gray)
+![Kubernetes](https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white&color=gray)
+![Jenkins](https://img.shields.io/badge/Jenkins-D24939?style=for-the-badge&logo=Jenkins&logoColor=white&color=gray)
+![Jira](https://img.shields.io/badge/Jira-0052CC?style=for-the-badge&logo=Jira&logoColor=white&color=gray)
+![Swift](https://img.shields.io/badge/swift-F54A2A?style=for-the-badge&logo=swift&logoColor=white&color=gray)
+![Ruby](https://img.shields.io/badge/ruby-%23CC342D.svg?style=for-the-badge&logo=ruby&logoColor=white&color=gray)
+![Rust](https://img.shields.io/badge/rust-%23000000.svg?style=for-the-badge&logo=rust&logoColor=white&color=gray)
+![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white&color=gray)
+![Vue.js](https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=white&color=gray)
+![Android Studio](https://img.shields.io/badge/Android%20Studio-3DDC84.svg?style=for-the-badge&logo=android-studio&logoColor=white&color=gray)
+![Kotlin](https://img.shields.io/badge/kotlin-%237F52FF.svg?style=for-the-badge&logo=kotlin&logoColor=white&color=gray) -->
+
+## Links
+
+- [Malt](https://www.malt.fr/profile/alexishenry)
+- [upwork](https://www.upwork.com/freelancers/~012fa432ae9ab58692)
+- [Wakatime](https://wakatime.com/@AlxisHenry)
+- [Codewars](https://www.codewars.com/users/AlxisHenry)
+- [Documentations](https://alxishenry.github.io/docs/)
+`;
