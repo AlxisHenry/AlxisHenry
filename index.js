5649fd814c0cf4f3f8b22165275239554142a020
Alexis HENRY <a.henry@opticod.fr>
2024-05-02 11:20:56 +0200

    fix: custom commit message

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-const { z } = require('zod');
-const { promises: fs } = require('fs');
-const readme = require('./readme.js');
+const { z } = require("zod");
+const { promises: fs } = require("fs");
+const readme = require("./readme.js");
 
 const averageSchema = z.object({
-  average: z.number().min(0).max(20),
+    average: z.number().min(0).max(20),
 });
 
-const updateReadme = (average) => {
-  const readmeContent = readme.replace(/{{average}}/g, average);
+const getCommit = (currentAverage, average) => {
+    let type = currentAverage < average ? "increase" : "decrease";
 
-  fs.writeFile('README.md', readmeContent)
-    .then(() => console.log('README.md updated'))
-    .catch((err) => console.error(err));
-}
+    return `${
+        type === "increase" ? "🚀" : "🔻"
+    } Average has ${type}d to ${average}/20`;
+};
+
+const updateReadme = async (average) => {
+    const currentReadme = await fs.readFile("README.md", "utf-8");
+    const readmeContent = readme.replace(/{{average}}/g, average);
+
+    fs.writeFile("README.md", readmeContent)
+        .then(() => {
+            const currentAverageSentence = currentReadme.match(
+                /My current average at Epitech is \d{1,2}\.\d{2}\/20/
+            )[0];
+
+            const currentAverage =
+                currentAverageSentence.match(/\d{1,2}\.\d{2}/)[0];
+
+            if (currentAverage === average) return;
+
+            let commit = getCommit(currentAverage, average);
+
+            fs.writeFile("commit.txt", commit).then(() => {
+                console.log(commit);
+            });
+        })
+        .catch((err) => console.error(err));
+};
 
 const main = () => {
-  fetch("https://epigrades.alexishenry.eu/api/average")
-    .then(response => response.json())
-    .then(data => {
-      let isAverageValid = averageSchema.safeParse(data);
+    fetch("https://epigrades.alexishenry.eu/api/average")
+        .then((response) => response.json())
+        .then(async (data) => {
+            let isAverageValid = averageSchema.safeParse(data);
 
-      if (isAverageValid.success) {
-        updateReadme(isAverageValid.data.average);
-      }
-    });
-}
+            if (isAverageValid.success) {
+                await updateReadme(isAverageValid.data.average.toString());
+            }
+        });
+};
 
 main();
